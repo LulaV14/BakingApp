3af798f9c3567dbaae870293879c60963d4ddaa3
LulaV14 <lulavillalobos@gmail.com>
2019-01-26 00:43:49 -0800

    make step row clickable

--- app/src/main/java/com/example/lulavillalobos/bakingapp/UI/DescriptionFragment.java
+++ app/src/main/java/com/example/lulavillalobos/bakingapp/UI/DescriptionFragment.java
@@ -7,8 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.lulavillalobos.bakingapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,16 +23,15 @@ import com.example.lulavillalobos.bakingapp.R;
  * create an instance of this fragment.
  */
 public class DescriptionFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String STEP_NUMBER_KEY = "step_number";
 
-    private OnFragmentInteractionListener mListener;
+    private String mStepNumber;
+
+//    private OnFragmentInteractionListener mListener;
+
+    @BindView(R.id.step_description_frag)
+    TextView tvStepNumber;
 
     public DescriptionFragment() {
         // Required empty public constructor
@@ -38,16 +41,14 @@ public class DescriptionFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param step_number Parameter 1.
      * @return A new instance of fragment DescriptionFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DescriptionFragment newInstance(String param1, String param2) {
+    public static DescriptionFragment newInstance(String step_number) {
         DescriptionFragment fragment = new DescriptionFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(STEP_NUMBER_KEY, step_number);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +57,7 @@ public class DescriptionFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mStepNumber = getArguments().getString(STEP_NUMBER_KEY);
         }
     }
 
@@ -65,31 +65,36 @@ public class DescriptionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_description, container, false);
+        View view = inflater.inflate(R.layout.fragment_description, container, false);
+        ButterKnife.bind(this, view);
+
+        tvStepNumber.setText(mStepNumber);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+//        mListener = null;
     }
 
     /**
@@ -102,8 +107,8 @@ public class DescriptionFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        void onFragmentInteraction(Uri uri);
+//    }
 }
--- app/src/main/java/com/example/lulavillalobos/bakingapp/UI/MainActivity.java
+++ app/src/main/java/com/example/lulavillalobos/bakingapp/UI/MainActivity.java
@@ -4,12 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.lulavillalobos.bakingapp.R;
+import com.example.lulavillalobos.bakingapp.dummy.DummyContent;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity implements StepsFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Bundle bundle = new Bundle();
+        bundle.putString("step_number", item.id);
+        DescriptionFragment descriptionFragment = new DescriptionFragment();
+        descriptionFragment.setArguments(bundle);
     }
 }
