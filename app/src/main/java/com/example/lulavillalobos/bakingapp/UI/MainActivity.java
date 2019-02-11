29753d3c54c50cd4e6b009d55f3a485bf8bebbef
LulaV14 <lulavillalobos@gmail.com>
2019-02-10 20:03:10 -0600

    start fresh

--- app/src/main/java/com/example/lulavillalobos/bakingapp/UI/MainActivity.java
+++ app/src/main/java/com/example/lulavillalobos/bakingapp/UI/MainActivity.java
@@ -4,23 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.lulavillalobos.bakingapp.R;
-import com.example.lulavillalobos.bakingapp.dummy.DummyContent;
 
-import butterknife.BindView;
-
-public class MainActivity extends AppCompatActivity implements StepsFragment.OnListFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public void onListFragmentInteraction(DummyContent.DummyItem item) {
-        Bundle bundle = new Bundle();
-        bundle.putString("step_number", item.id);
-        DescriptionFragment descriptionFragment = new DescriptionFragment();
-        descriptionFragment.setArguments(bundle);
     }
 }
